3e75a584aa79b91a58adb02d025e4e7d3d405015
weiqizheng <weiqizheng@gmail.com>
2024-12-24 09:59:46 +0800

    Day 2

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,35 @@
-use std::net::Ipv4Addr;
+use std::net::{Ipv4Addr, Ipv6Addr};
 
 use axum::{
     body::Body,
     extract::Query,
     http::{header::LOCATION, HeaderValue, Response, StatusCode},
-    response::IntoResponse,
+    response::{Html, IntoResponse},
     routing::get,
     Router,
 };
 use serde::Deserialize;
 
-async fn hello_world() -> &'static str {
+#[shuttle_runtime::main]
+async fn main() -> shuttle_axum::ShuttleAxum {
+    let router = Router::new()
+        .route("/", get(day_1_hello_world))
+        .route("/-1/seek", get(day_1_seek))
+        .route("/2/dest", get(day_2_dest))
+        .route("/2/key", get(day_2_key))
+        .route("/2/v6/dest", get(day_2_v6_dest))
+        .route("/2/v6/key", get(day_2_v6_key));
+
+    Ok(router.into())
+}
+
+// day -1
+
+async fn day_1_hello_world() -> &'static str {
     "Hello, bird!"
 }
 
-async fn seek() -> impl IntoResponse {
+async fn day_1_seek() -> impl IntoResponse {
     let mut response = Response::new(Body::empty());
     *response.status_mut() = StatusCode::FOUND;
     response.headers_mut().insert(
@@ -24,27 +39,62 @@ async fn seek() -> impl IntoResponse {
     response
 }
 
-// #[derive(Deserialize)]
-// struct Day2Task1Query {
-//     from: String,
-//     key: String,
-// }
+// day 2
 
-// async fn day_2_task_1(query: Query<Day2Task1Query>) -> impl IntoResponse {
-//     let from = query.from.parse::<Ipv4Addr>().unwrap();
-//     let key = query.key.parse::<Ipv4Addr>().unwrap();
-//     from.octets()
-//         .into_iter()
-//         .zip(key.octets())
-//         .for_each(|(mut from, key)| from += key);
-//     println!("from {from}");
-// }
+#[derive(Deserialize)]
+struct Day2DestQuery {
+    from: Ipv4Addr,
+    key: Ipv4Addr,
+}
 
-#[shuttle_runtime::main]
-async fn main() -> shuttle_axum::ShuttleAxum {
-    let router = Router::new()
-        .route("/", get(hello_world))
-        .route("/-1/seek", get(seek));
+async fn day_2_dest(query: Query<Day2DestQuery>) -> impl IntoResponse {
+    let mut from = query.from.octets();
+    let key = query.key;
+    from.iter_mut()
+        .zip(key.octets())
+        .for_each(|(from, key)| *from = from.wrapping_add(key));
+    let dest = Ipv4Addr::new(from[0], from[1], from[2], from[3]);
 
-    Ok(router.into())
+    Html(dest.to_string())
+}
+
+#[derive(Deserialize)]
+struct Day2KeyQuery {
+    from: Ipv4Addr,
+    to: Ipv4Addr,
+}
+
+async fn day_2_key(query: Query<Day2KeyQuery>) -> impl IntoResponse {
+    let mut to = query.to.octets();
+    let from = query.from;
+    to.iter_mut()
+        .zip(from.octets())
+        .for_each(|(to, from)| *to = to.wrapping_sub(from));
+
+    let key = Ipv4Addr::new(to[0], to[1], to[2], to[3]);
+    Html(key.to_string())
+}
+
+#[derive(Deserialize)]
+struct Day2V6DestQuery {
+    from: Ipv6Addr,
+    key: Ipv6Addr,
+}
+
+async fn day_2_v6_dest(query: Query<Day2V6DestQuery>) -> impl IntoResponse {
+    let dest = Ipv6Addr::from_bits(query.from.to_bits() ^ query.key.to_bits());
+
+    Html(dest.to_string())
+}
+
+#[derive(Deserialize)]
+struct Day2V6KeyQuery {
+    from: Ipv6Addr,
+    to: Ipv6Addr,
+}
+
+async fn day_2_v6_key(query: Query<Day2V6KeyQuery>) -> impl IntoResponse {
+    let key = Ipv6Addr::from_bits(query.from.to_bits() ^ query.to.to_bits());
+
+    Html(key.to_string())
 }
